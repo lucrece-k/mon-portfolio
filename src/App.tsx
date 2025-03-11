a9dac640d4311825c47a81ad4693af11cb723c8d
kouame lucrèce <kouamelucrece@gmail.com>
2025-03-11 15:33:01 +0100

    firt commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./styles.scss";
+function App() {
+  return (
+    <div>
+      <h1>Bienvenue sur mon Portfolio</h1>
+      <p>En cours de développement...</p>
+    </div>
+  );
+}
+
+export default App;
