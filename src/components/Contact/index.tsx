1578a83a10ab5b7b2b6127d8b1a72d5540eb09ee
kouame lucrèce <kouamelucrece@gmail.com>
2025-04-30 23:49:27 +0200

    profil, projets and contact update

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -5,8 +5,8 @@ export default function Contact() {
       <h1>Contactez-moi</h1>
       <div className="contact_contente">
         Vous avez une opportunité à me proposer ou simplement une question ?
-        N’hésitez pas à me contacter ! Je serais ravie d’échanger avec vous.
-        Cliquez sur le bouton ci-dessous pour m’envoyer un email, et je vous
+        N'hésitez pas à me contacter ! Je serais ravie d'échanger avec vous.
+        Cliquez sur le bouton ci-dessous pour m'envoyer un email, et je vous
         répondrai dans les plus brefs délais.
         <a href="mailto:kouamelucrece@gmail.com">Email!</a>
       </div>
--- src/components/Profil/index.tsx
+++ src/components/Profil/index.tsx
@@ -5,7 +5,7 @@ function Profil() {
       <h1>A propos de moi</h1>
       <div className="profil-contenu">
         <p>
-          "Hello ! Je m'appelle KOUAME Lucrèce, intégratrice web junior. Pendant
+          Hello ! Je m'appelle KOUAME Lucrèce, intégratrice web junior. Pendant
           plusieurs années, j’ai exercé en tant que technicienne de laboratoire
           dans le secteur de la chimie, ce qui m’a permis d’acquérir des
           compétences essentielles comme la rigueur, l’organisation et le
@@ -16,7 +16,7 @@ function Profil() {
           solides en HTML, CSS, JavaScript et React. Aujourd’hui, je suis à la
           recherche d’un premier poste dans lequel je pourrai mettre mes
           compétences à profit et continuer à progresser au sein d’une équipe
-          dynamique !"
+          dynamique !
         </p>
       </div>
     </div>
